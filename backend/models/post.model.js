5c663492f30e06ce1c8d56e886a02ef173439ca6
SyedYasirAliZafar <yasirali1234zafar@gmail.com>
2025-11-29 00:59:51 +0500

    Models Created

---
+++ backend/models/post.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Schema } from "mongoose";
+
+const postSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+    },
+    desc: {
+      type: String,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean,
+      default: false,
+    },
+    visit: {
+      type: Number,
+      default: 0,
+    },
+    savedPosts: {
+      type: [String],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = mongoose.model("Post", postSchema);
---
+++ backend/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    img:{
+        type: String
+    },
+    savedPosts:{
+        type:[String],
+        default:[]
+    },
+}, {timestamps: true});
+
+export const User = mongoose.model("User", userSchema);
